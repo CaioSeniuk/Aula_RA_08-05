d564f4f32eebfa7411a928fe77ba68f02793f439
Caio Henrique Goncalves Seniuk <caio.seniuk@pucpr.edu.br>
2024-05-08 11:38:22 -0300

    v2

--- inverter palavra.py
+++ inverter palavra.py
@@ -1,7 +1,13 @@
-palavra_lista = []
+'''palavra_lista = []
 palavra = input("\nDigite uma palavra: ")
 for i in palavra:
     palavra_lista.append(i)
 palavra_lista.reverse()
 for x in palavra_lista:
-    print(x,end="")
+    print(x,end="")'''
+
+def inverter_palavra(palavra):
+    return palavra[::-1]
+
+palavra = input("Digite uma palavra: ")
+print("Palavra invertida:", inverter_palavra(palavra))
