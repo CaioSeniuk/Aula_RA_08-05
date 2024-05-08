5f23440abe50cab6ed11d626e3dd6ed62c4a4b98
Caio Henrique Goncalves Seniuk <caio.seniuk@pucpr.edu.br>
2024-05-08 12:22:11 -0300

    dados pokemons

--- inverter palavra.py
+++ inverter palavra.py
@@ -6,8 +6,16 @@ palavra_lista.reverse()
 for x in palavra_lista:
     print(x,end="")'''
 
-def inverter_palavra(palavra):
+'''def inverter_palavra(palavra):
     return palavra[::-1]
 
 palavra = input("Digite uma palavra: ")
-print("Palavra invertida:", inverter_palavra(palavra))
+print("Palavra invertida:", inverter_palavra(palavra))'''
+
+palavra = input("Digite uma palavra: ")
+palavra_invertida = ""
+
+for i in range(len(palavra) - 1, -1, -1):
+    palavra_invertida += palavra[i]
+
+print("Palavra invertida:", palavra_invertida)
---
+++ pokemons teste/pokemon.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+
+dados = pd.read_csv("pokemons teste/pokemon.csv")
+
+print(dados)
