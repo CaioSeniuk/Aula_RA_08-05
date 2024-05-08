fe75db53303a66aa3c33ee5d70cab826e931b86e
Caio Henrique Goncalves Seniuk <caio.seniuk@pucpr.edu.br>
2024-05-08 12:23:58 -0300

    v2

--- inverter palavra.py
+++ inverter palavra.py
@@ -18,4 +18,4 @@ palavra_invertida = ""
 for i in range(len(palavra) - 1, -1, -1):
     palavra_invertida += palavra[i]
 
-print("Palavra invertida:", palavra_invertida)
+print("Palavra invertida:", palavra_invertida
